3f5f93ee9fffcb6b65de7a0c9bdfad2a191de495
AlmantasK <almantusk@gmail.com>
2024-12-06 11:49:49 +0200

    site: paths

--- Website/next.config.js
+++ Website/next.config.js
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV == 'production'
+
 const nextConfig = {
+  basePath: isProd ? '/dnd-zemaiciai' : '',
   output: 'export',
   distDir: 'dist',
   images: {
